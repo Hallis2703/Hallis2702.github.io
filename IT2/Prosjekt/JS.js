7ee921ac232d10e8f9f5baad4e7a7e291252dfba
Halvor Riddervold <halvor.g.riddervold@outlook.com>
2018-11-24 17:42:06 +0100

    SUP

---
+++ IT2/Prosjekt/JS.js
@@ -0,0 +1,16 @@
+
+
+
+
+var snake = document.getElementById("Snake");
+
+window.onmouseover = function (event) {
+    snake.style.backgroundImage = 'url("Screenshot 2018-11-23 at 10.59.56.png")';
+    snake.style.backgroundSize = 'cover';
+}
+
+
+
+
+
+
